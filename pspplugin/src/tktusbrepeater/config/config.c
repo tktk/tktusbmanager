5a10c0ac8bc508cff1b3ddc357e4d29e1e913124
tekuto <g.tekuto@gmail.com>
2023-04-05 16:05:12 +0900

    loadConfigFile()の処理を修正

--- pspplugin/src/tktusbrepeater/config/config.c
+++ pspplugin/src/tktusbrepeater/config/config.c
@@ -1,16 +1,8 @@
 #include "tktusbrepeater/config/config.h"
+#include "tktusbrepeater/common/file.h"
+#include "tktusbrepeater/common/memory.h"
 #include "parson/parson.h"
-#include <pspiofilemgr.h>
-#include <pspsysmem.h>
-#include <string.h>
-
-#define PARTITION_NAME_CONTENTS "contents"
-
-enum {
-    BUFFER_SIZE = 512,
-
-    TAIL = '\0',
-};
+#include <pspkerneltypes.h>
 
 /*
 static TktUsbRepeaterConfig * parseConfigJson(
@@ -26,136 +18,6 @@ static TktUsbRepeaterConfig * parseConfigJson(
     return config;
 }
 */
-
-static int allocContents(
-    SceUID *        _contentsIdPtr
-    , char **       _contentsPtr
-    , const SceSize _CONTENTS_SIZE
-)
-{
-    const SceUID    CONTENTS_ID = sceKernelAllocPartitionMemory(
-        2
-        , PARTITION_NAME_CONTENTS
-        , PSP_SMEM_Low
-        , _CONTENTS_SIZE
-        , NULL
-    );
-    if( CONTENTS_ID < 0 ) {
-        return 1;
-    }
-
-    *_contentsIdPtr = CONTENTS_ID;
-    *_contentsPtr = sceKernelGetBlockHeadAddr( CONTENTS_ID );
-
-    return 0;
-}
-
-static int reallocContents(
-    SceUID *        _contentsIdPtr
-    , char **       _contentsPtr
-    , const SceSize _CONTENTS_SIZE
-    , const SceSize _NEW_CONTENTS_SIZE
-)
-{
-    SceUID  newContentsId;
-    char *  newContents;
-
-    const int   RESULT = allocContents(
-        &newContentsId
-        , &newContents
-        , _NEW_CONTENTS_SIZE
-    );
-    if( RESULT != 0 ) {
-        return RESULT;
-    }
-
-    memcpy(
-        newContents
-        , *_contentsPtr
-        , _CONTENTS_SIZE
-    );
-
-    sceKernelFreePartitionMemory( *_contentsIdPtr );
-
-    *_contentsIdPtr = newContentsId;
-    *_contentsPtr = newContents;
-
-    return 0;
-}
-
-static int readFileLoop(
-    SceUID *        _contentsIdPtr
-    , char **       _contentsPtr
-    , const SceUID  _FILE_ID
-)
-{
-    int result;
-
-    SceSize contentsSize = BUFFER_SIZE;
-
-    result = allocContents(
-        _contentsIdPtr
-        , _contentsPtr
-        , contentsSize
-    );
-    if( result != 0 ) {
-        return result;
-    }
-
-    while( 1 ) {
-        const int   READ_SIZE = sceIoRead(
-            _FILE_ID
-            , ( *_contentsPtr ) + ( contentsSize - BUFFER_SIZE )
-            , BUFFER_SIZE
-        );
-        if( READ_SIZE < BUFFER_SIZE ) {
-            contentsSize = contentsSize - BUFFER_SIZE + READ_SIZE;
-
-            break;
-        }
-
-        const int   NEW_CONTENTS_SIZE = contentsSize + BUFFER_SIZE;
-
-        reallocContents(
-            _contentsIdPtr
-            , _contentsPtr
-            , contentsSize
-            , NEW_CONTENTS_SIZE
-        );
-
-        contentsSize = NEW_CONTENTS_SIZE;
-    }
-
-    *( ( *_contentsPtr ) + contentsSize ) = TAIL;
-
-    return 0;
-}
-
-static int readFile(
-    SceUID *        _contentsIdPtr
-    , char **       _contentsPtr
-    , const char *  _FILE_PATH
-)
-{
-    const SceUID    FILE_ID = sceIoOpen(
-        _FILE_PATH
-        , PSP_O_RDONLY
-        , 0777
-    );
-    if( FILE_ID <= 0 ) {
-        return 1;
-    }
-
-    const int   RESULT = readFileLoop(
-        _contentsIdPtr
-        , _contentsPtr
-        , FILE_ID
-    );
-
-    sceIoClose( FILE_ID );
-
-    return RESULT;
-}
 
 static int parseConfigFile(
     TktUsbRepeaterConfig *  _config
@@ -207,7 +69,7 @@ int loadConfigFile(
         );
     }
 
-    sceKernelFreePartitionMemory( contentsId );
+    freeMemory( contentsId );
 
     return result;
 }
