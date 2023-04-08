69b26e35eea52b5af2266e8874f00451ab94e5b5
tekuto <g.tekuto@gmail.com>
2023-04-08 21:20:09 +0900

    以下の関数の処理を実装
    ・allocEndpoints()
    ・freeEndpoints()

--- pspplugin/src/tktusbrepeater/usb/endpoints.c
+++ pspplugin/src/tktusbrepeater/usb/endpoints.c
@@ -1,4 +1,7 @@
 #include "tktusbrepeater/usb/endpoints.h"
+#include "tktusbrepeater/usb/endpoint.h"
+#include "tktusbrepeater/common/memory.h"
+#include <pspkerneltypes.h>
 #include <stddef.h>
 
 int allocEndpoints(
@@ -6,7 +9,24 @@ int allocEndpoints(
     , size_t            _ENDPOINTS_COUNT
 )
 {
-    //TODO
+    SceUID  endpointsId = allocMemory( _ENDPOINTS_COUNT * sizeof( *( _endpoints->endpoints ) ) );
+    if( endpointsId < 0 ) {
+        return 1;
+    }
+
+    TktUsbEndpoint *    endpoints = ( TktUsbEndpoint * )getMemoryAddress( endpointsId );
+
+    size_t  i;
+    for( i = 0 ; i < _ENDPOINTS_COUNT ; i++ ) {
+        TktUsbEndpoint *    endpoint = endpoints + i;
+
+        endpoint->nameId = 0;
+    }
+
+    _endpoints->endpointsId = endpointsId;
+    _endpoints->endpointsCount = _ENDPOINTS_COUNT;
+    _endpoints->endpoints = endpoints;
+
     return 0;
 }
 
@@ -14,5 +34,16 @@ void freeEndpoints(
     TktUsbEndpoints *   _endpoints
 )
 {
-    //TODO
+    size_t  i;
+    for( i = 0 ; i < _endpoints->endpointsCount ; i++ ) {
+        TktUsbEndpoint *    endpoint = _endpoints->endpoints + i;
+
+        freeEndpoint( endpoint );
+    }
+
+    _endpoints->endpointsCount = 0;
+
+    freeMemory( _endpoints->endpointsId );
+
+    _endpoints->endpointsId = 0;
 }
